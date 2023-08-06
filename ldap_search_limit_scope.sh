3233dfc345fafa21429792c3a720f690012e0980
Srikanth Josyula <44222500+srikanth-josyula@users.noreply.github.com>
2023-08-06 12:19:16 -0300

    Create ldap_search_limit_scope.sh

---
+++ ldap_search_limit_scope.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# LDAP Search with Limiting Search Scope
+# Demonstrates searching within a specific subtree of the directory.
+
+#This script includes comments explaining the purpose of the script and demonstrates how to perform an LDAP search within a specific subtree of the directory using the -b option. The provided example showcases searching for entries with objectClass=person within the "ou=Users,dc=example,dc=com" search base and retrieving the specified attributes. I
+
+# Connection Details
+HOST="ldap.forumsys.com"
+PORT=389
+BIND_DN="cn=read-only-admin,dc=example,dc=com"
+ADMIN_PASSWORD="password"
+
+# Search with Limiting Search Scope
+SEARCH_BASE="ou=Users,dc=example,dc=com"
+FILTER="(objectClass=person)"
+ATTRIBUTES="cn mail"
+
+# Running the ldapsearch command with limiting search scope
+ldapsearch -x -h $HOST -p $PORT -D "$BIND_DN" -w "$ADMIN_PASSWORD" -b "$SEARCH_BASE" "$FILTER" $ATTRIBUTES
+
+<<OUTPUT
+Sample Output:
+
+# extended LDIF
+#
+# LDAPv3
+# base <ou=Users,dc=example,dc=com> with scope subtree
+# filter: (objectClass=person)
+# requesting: cn mail
+#
+
+# search result
+search: 2
+result: 0 Success
+
+# numResponses: 3
+# numEntries: 2
+
+dn: uid=john,ou=Users,dc=example,dc=com
+cn: John Doe
+mail: johndoe@example.com
+
+dn: uid=emma,ou=Users,dc=example,dc=com
+cn: Emma Johnson
+mail: emmajohnson@example.com
+OUTPUT
