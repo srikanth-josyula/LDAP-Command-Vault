f69a4847ede953a8336f9d656d6958e9a9281b90
Srikanth Josyula <44222500+srikanth-josyula@users.noreply.github.com>
2023-08-06 11:43:52 -0300

    Update ldap_search_admin.sh

--- ldap_search_admin.sh
+++ ldap_search_admin.sh
@@ -2,6 +2,7 @@
 # LDAP Search Example with Admin Credentials
 # Demonstrates using the ldapsearch command with admin credentials for additional information.
 
+# Connection Details
 HOST="ldap.forumsys.com"
 PORT=389
 BIND_DN="cn=read-only-admin,dc=example,dc=com"
@@ -9,4 +10,57 @@ ADMIN_PASSWORD="password"
 BASE_DN="dc=example,dc=com"
 
 # Running the ldapsearch command with admin credentials
+# The following command searches for entries in the specified base DN with the objectclass=* filter.
+# It retrieves the common name (cn) and user ID (uid) attributes.
 ldapsearch -x -h $HOST -p $PORT -D "$BIND_DN" -w "$ADMIN_PASSWORD" -b "$BASE_DN" "(objectclass=*)" cn uid
+
+<<EOF
+Sample Output:
+
+# extended LDIF
+#
+# LDAPv3
+# base <dc=example,dc=com> with scope subtree
+# filter: (objectclass=*)
+# requesting: cn uid
+#
+
+# search result
+search: 2
+result: 0 Success
+
+# numResponses: 5
+# numEntries: 4
+
+dn: uid=pasteur,dc=example,dc=com
+cn: Louis Pasteur
+uid: pasteur
+objectClass: inetOrgPerson
+objectClass: organizationalPerson
+objectClass: person
+objectClass: top
+
+dn: uid=nobel,dc=example,dc=com
+cn: Alfred Nobel
+uid: nobel
+objectClass: inetOrgPerson
+objectClass: organizationalPerson
+objectClass: person
+objectClass: top
+
+dn: uid=curie,dc=example,dc=com
+cn: Marie Curie
+uid: curie
+objectClass: inetOrgPerson
+objectClass: organizationalPerson
+objectClass: person
+objectClass: top
+
+dn: uid=boyle,dc=example,dc=com
+cn: Robert Boyle
+uid: boyle
+objectClass: inetOrgPerson
+objectClass: organizationalPerson
+objectClass: person
+objectClass: top
+EOF
