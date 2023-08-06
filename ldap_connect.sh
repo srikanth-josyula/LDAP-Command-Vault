1c8ca1a3add5fd7bddbd74cfc37a1708cf4689fe
Srikanth Josyula <44222500+srikanth-josyula@users.noreply.github.com>
2023-08-06 11:35:51 -0300

    Update ldap_connect.sh

--- ldap_connect.sh
+++ ldap_connect.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-# LDAP Connection Script
-# Connects to the online LDAP test server using provided credentials.
+# LDAP Connection Test Script
+# This script tests the connection to an LDAP server without performing a search.
 
-HOST="ldap.forumsys.com"
+#In this script, the -b parameter is set to an empty value, and the search filter is set to (objectClass=*). The -s base flag specifies that the search should be performed at the base level. The attribute 1.1 is used to specify that no attributes should be returned (it's a common way to perform a simple connection test).
+
+# Connection Details
+HOSTNAME="ldap.forumsys.com"
 PORT=389
 BIND_DN="cn=read-only-admin,dc=example,dc=com"
 BIND_PASSWORD="password"
 
-# Running the ldapsearch command to test connection
-ldapsearch -x -h $HOST -p $PORT -D "$BIND_DN" -w "$BIND_PASSWORD" -b "dc=example,dc=com" -s base "(objectclass=*)" namingContexts
+# Running the LDAP connection test
+ldapsearch -x -H ldap://${HOSTNAME}:${PORT} -D "${BIND_DN}" -w "${BIND_PASSWORD}" -b "" -s base "(objectClass=*)" 1.1
